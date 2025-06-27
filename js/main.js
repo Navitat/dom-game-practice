0d43478a11de58960f613ea76c19cc43d76f7175
navitat <anlon636@gmail.com>
2025-06-27 11:05:01 +0200

    add starting css points for player from main.js class

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
 class Player {
   constructor() {
-    this.width = 20;
-    this.height = 20;
-    this.positionX = 20;
-    this.positionY = 20;
+    this.width = 30;
+    this.height = 10;
+    this.positionX = 40;
+    this.positionY = 0;
+
+    //Starting CSS positions and assignment relying on class values (not in the css file)
+    const playerElm = document.getElementById("player");
+    playerElm.style.left = this.positionX + "vw";
+    playerElm.style.bottom = this.positionY + "vh";
+    playerElm.style.width = this.width + "vw";
+    playerElm.style.height = this.height + "vh";
   }
 
   moveLeft() {
-    this.positionX--;
-    console.log(`moving left...${this.positionX}`);
+    this.positionX--; // this takes care of moving left
+
+    const playerElm = document.getElementById("player");
+    playerElm.style.left = this.positionX + "vw"; // remember to update the UI
   }
   moveRight() {
-    this.positionX++;
-    console.log(`moving right...${this.positionX}`);
+    this.positionX++; // this takes care of moving left
+
+    const playerElm = document.getElementById("player");
+    playerElm.style.left = this.positionX + "vw"; // remember to update the UI
   }
 }
 
