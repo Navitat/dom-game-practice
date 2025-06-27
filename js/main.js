235281a88b6a785add5d25344ccb0bc7e4ecc5fd
navitat <anlon636@gmail.com>
2025-06-27 13:03:22 +0200

    feat: implement obstacles generation, movement and deletion

--- js/main.js
+++ js/main.js
@@ -19,16 +19,77 @@ class Player {
 
   moveLeft() {
     this.positionX--; // this takes care of moving left
+    if (this.positionX < 0) {
+      this.positionX = 0;
+    }
     this.updateUI();
   }
   moveRight() {
-    this.positionX++; // this takes care of moving left
+    this.positionX++; // this takes care of moving right
+    if (this.positionX > 70) {
+      this.positionX = 70;
+    }
+    this.updateUI();
+  }
+}
+
+class Obstacle {
+  constructor() {
+    this.width = 20;
+    this.height = 10;
+    this.positionX = 30;
+    this.positionY = 100; //vh
+    this.obstacleElm = null;
+
+    this.createDomElement();
+  }
+
+  createDomElement() {
+    //step 1: create element
+    this.obstacleElm = document.createElement("div");
+
+    //step 2: add content or modify (ex: innerHTML)
+    this.obstacleElm.className = "obstacle";
+
+    //Step 3: append to dom (parentElm.appendChild())
+    const parentElm = document.getElementById("board");
+    parentElm.appendChild(this.obstacleElm);
+  }
+
+  updateUI() {
+    this.obstacleElm.style.left = this.positionX + "vw";
+    this.obstacleElm.style.bottom = this.positionY + "vh";
+    this.obstacleElm.style.width = this.width + "vw";
+    this.obstacleElm.style.height = this.height + "vh";
+  }
+
+  moveDown() {
+    this.positionY--;
+    if (this.positionY < 0) {
+      this.obstacleElm.remove();
+    }
     this.updateUI();
   }
 }
 
 //Create an instance of the class player
 const player = new Player();
+
+//create instance of obstacle and setInterval
+
+//create obstacles
+const obstaclesArr = []; // stores instances of the class Obstacle
+setInterval(() => {
+  const newObstacle = new Obstacle();
+  obstaclesArr.push(newObstacle);
+}, 3_000);
+
+//move obstacles
+setInterval(() => {
+  obstaclesArr.forEach((obstacleInstance) => {
+    obstacleInstance.moveDown();
+  });
+}, 4_0);
 
 // Event listeners
 document.addEventListener("keydown", (event) => {
--- planning.md
+++ planning.md
@@ -3,7 +3,7 @@
 1. Milestone 1: user can move the player left/right
 
 - [x] Initial setup: (html, css, js, git repo)
-- [ ] create Player class (js class for functionality and information)
+- [x] create Player class (js class for functionality and information)
   - width
   - height
   - positionX
@@ -13,8 +13,20 @@
 - [x] add event listeners
 
 2. Milestone 2: obstacles appearing in the UI + obstacle move
+
+- One obstacle:
+  - [ ] create obstacle class (js class)
+    - positionX
+    - positionY
+    - width
+    - height
+    - moveDown()
+  - [ ] create interval to move obstacle down
+
 3. Milestone 3: detect if thereis collision
 4. Milestone 4: make the game more interesting & fix bugs
 
 - colors
 - points
+- random size for obstacles
+- obstacles with different speeds
