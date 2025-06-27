90a56d7bba0fd6a0c9cb75f72af8c0a5863862af
navitat <anlon636@gmail.com>
2025-06-27 10:33:48 +0200

    create initial board size and player initial position

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log("Helloo");
+class Player {
+  constructor() {
+    this.width = 20;
+    this.height = 20;
+    this.positionX = 20;
+    this.positionY = 20;
+  }
+
+  moveLeft() {
+    this.positionX--;
+    console.log(`moving left...${this.positionX}`);
+  }
+  moveRight() {
+    this.positionX++;
+    console.log(`moving right...${this.positionX}`);
+  }
+}
+
+//Create an instance of the class player
+const player = new Player();
+
+// Event listeners
+document.addEventListener("keydown", (event) => {
+  if (event.code === "ArrowLeft") {
+    player.moveLeft();
+  } else if (event.code === "ArrowRight") {
+    player.moveRight();
+  }
+});
--- planning.md
+++ planning.md
@@ -2,7 +2,7 @@
 
 1. Milestone 1: user can move the player left/right
 
-- [ ] Initial setup: (html, css, js, git repo)
+- [x] Initial setup: (html, css, js, git repo)
 - [ ] create Player class (js class for functionality and information)
   - width
   - height
@@ -13,8 +13,8 @@
 - [ ] add event listeners
 
 2. Milestone 2: obstacles appearing in the UI + obstacle move
-3. Mielstone 3: detect if thereis collision
-4. milestone 4: make the game more interesting & fix bugs
+3. Milestone 3: detect if thereis collision
+4. Milestone 4: make the game more interesting & fix bugs
 
 - colors
 - points
